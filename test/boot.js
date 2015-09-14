8b451201af506825f6b1c0e831e9546ba95bc421
Scott Davis <Scott.Davis@Webtrends.com>
2015-09-14 15:11:29 -0700

    initial unit test

--- test/boot.js
+++ test/boot.js
@@ -1,15 +1,10 @@
-// var sandbox = require('nodeunit').utils.sandbox;
-// // pass in some node globals
-// var box_globals = {
-//     // Passing module.exports into the sandbox will give your code  access to it.
-//     module: {exports: exports},
-//     // Passing require into the sandbox will give your code  access to use it AND
-//     // will share the cache with modules already required from outside the sandbox.
-//     require: require,
-//     // Passing console into the sandbox will give your code access to it
-//     console: console
-// };
+// try {
+//     var reporter = require('nodeunit').reporters.default;
+// }
+// catch(e) {
+//     console.log("Cannot find nodeunit module.");
+//     process.exit();
+// }
 
-// var du = sandbox('lib/discovery.js', box_globals);
-
-
+// process.chdir(__dirname);
+// reporter.run(['sanity-check']);
