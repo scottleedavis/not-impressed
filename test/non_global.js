cd565a1dd949c96c8112960d95c75160b2f4b289
Scott Davis <Scott.Davis@Webtrends.com>
2015-10-11 11:14:51 -0700

    non global test

--- test/non_global.js
+++ test/non_global.js
@@ -1,11 +1,11 @@
-var assert = require('assert'),
-    pubsub = require('node-internal-pubsub'),
-    non_global = require('../examples/non_global');
+// var assert = require('assert'),
+//     pubsub = require('node-internal-pubsub'),
+//     non_global = require('../examples/non_global');
 
 
-describe('examples/non_global', function(){
-  it('runs', function(){
-    assert(typeof non_global != "undefined");
-  });
+// describe('examples/non_global', function(){
+//   it('runs', function(){
+//     assert(typeof non_global != "undefined");
+//   });
 
-});
+// });
