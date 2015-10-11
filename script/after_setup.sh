b26df03462a1a1622f9962588ccbc56f94b77b06
Scott Davis <Scott.Davis@Webtrends.com>
2015-10-11 10:46:08 -0700

    bug in coverage script call.

--- script/after_setup.sh
+++ script/after_setup.sh
@@ -5,5 +5,5 @@ a=$(node --version)
 
 if [ "$a" == "v0.12.7" ]
 then
-	cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+	cat ../coverage/lcov.info | ../node_modules/coveralls/bin/coveralls.js
 fi
