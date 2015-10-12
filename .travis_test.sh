5616370b98cc41f5e2c888258bf3edafb3f3c6a3
Scott Davis <Scott.Davis@Webtrends.com>
2015-10-11 19:38:08 -0700

    travis test.

--- .travis_test.sh
+++ .travis_test.sh
@@ -4,10 +4,10 @@ a=$(node --version)
 
 if [ "$a" == "v0.12.7" ]
 then
+	npm install coveralls ; 
 	istanbul cover _mocha -- -R spec;
-	npm install coveralls
-	echo "reporting coverage.."
-	cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
-	echo "done."
+	echo "reporting coverage..";
+	cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js ;
+	echo "done.";
 
 fi
